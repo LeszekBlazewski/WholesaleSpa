f301862a246eb9615513f9e269bd009325b5e861
Leszek Błażewski <blazewski.leszek@gmail.com>
2020-01-14 17:14:37 +0100

    Removed mock from admin history, fixed typo

--- src/app/pages/admin-order-history-page/admin-order-history-page.component.ts
+++ src/app/pages/admin-order-history-page/admin-order-history-page.component.ts
@@ -1,33 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Order } from 'src/app/models/Order';
-import { OrderStatus } from 'src/app/models/enums/OrderStatus';
 import { OrderService } from 'src/app/services/order.service';
-
-const now: Date = new Date();
-
-const FAKE_DATA: Order[] = [
-  {
-    date: now,
-    status: OrderStatus.created,
-    orderDetails: [{ product: { name: 'nike low', price: 123.50, category: { name: 'Shoes' } }, amount: 2 },
-    { product: { name: 'nike low', price: 123.50, category: { name: 'Shoes' } }, amount: 2 }],
-    totalValue: 1
-  },
-  {
-    date: new Date(now.getDate() - 1),
-    status: OrderStatus.canceled,
-    orderDetails: [{ product: { name: 'Redbull', price: 62.50, category: { name: 'Shoes' } }, amount: 2 }],
-    totalValue: 3
-  },
-  {
-    date: new Date(now.getDate() - 2),
-    status: OrderStatus.inProgress,
-    orderDetails: [{ product: { name: 'nike low', price: 123.50, category: { name: 'Shoes' } }, amount: 2 }],
-    totalValue: 2
-  },
-]
-
 
 
 @Component({
@@ -44,10 +18,8 @@ export class AdminOrderHistoryPageComponent implements OnInit {
 
   ngOnInit() {
 
-    //this.orders$ = this.orderService.getAllOrders();
+    this.orders$ = this.orderService.getAllOrders();
 
-    // Moq
-    this.orders$ = of(FAKE_DATA);
   }
 
 }
